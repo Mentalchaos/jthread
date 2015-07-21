ad24b63bf5745327b3a9d4b5a653381399653cfe
Sebastian Real <seba.real.a@gmail.com>
2015-07-20 23:51:39 -0300

    creating javascript project with web workers

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+var jshint = require('gulp-jshint');
+var tasks, server;
+
+tasks = {
+  //lint js file
+  lint: function(){
+    // return gulp.src('./lib/jthead.js')
+    //   .pipe(jshint())
+    //   .pipe(jshint.reporter('default'));
+    console.log('hinting');
+  },
+  //compress js files
+  compress: function(){
+    console.log('compressing file');
+  },
+  //serving out server for testing purposes
+  server: function(){
+    var server = gls.static(['app', 'js'], 3000);
+    server.start();
+  },
+  //watch for file changes
+  watch: function(){
+    gulp.watch([ './js/jthread.js' ], [ 'lint', 'compress' ]);
+  }
+};
+
+gulp.task('lint', tasks.lint);
+gulp.task('compress', tasks.compress);
+gulp.task('server', tasks.server);
+gulp.task('watch', tasks.watch);
+gulp.task('default', [ 'server', 'lint', 'compress', 'watch' ]);
